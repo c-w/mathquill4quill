50688354567bc1d144cadbd5782a62614e02ae36
Clemens Wolff <clewolff@microsoft.com>
2019-06-26 12:33:03 -0400

    Ensure demo page setup happens when page is ready

--- index.js
+++ index.js
@@ -1,45 +1,47 @@
 /* eslint-env browser */
 
 (function($, Quill) {
-  var quillOptions = {
-    modules: {
-      formula: true,
-      toolbar: [["bold", "italic", "underline", "formula"]]
-    },
-    placeholder:
-      "Type text here, or click on the formula button to enter math.",
-    theme: "snow"
-  };
+  $(document).ready(function() {
+    var quillOptions = {
+      modules: {
+        formula: true,
+        toolbar: [["bold", "italic", "underline", "formula"]]
+      },
+      placeholder:
+        "Type text here, or click on the formula button to enter math.",
+      theme: "snow"
+    };
 
-  var options = {};
+    var options = {};
 
-  $("input[type='checkbox'].option").each(function() {
-    var $option = $(this);
-    var name = $option.attr("data-name");
-    var isOptionEnabled =
-      window.location.href.indexOf("&" + name + "=true") !== -1;
+    $("input[type='checkbox'].option").each(function() {
+      var $option = $(this);
+      var name = $option.attr("data-name");
+      var isOptionEnabled =
+        window.location.href.indexOf("&" + name + "=true") !== -1;
 
-    if (isOptionEnabled) {
-      $option.prop("checked", true);
-      options[name] = JSON.parse($option.attr("data-value"));
-    }
-
-    $option.change(function(event) {
-      var url = window.location.href;
-      var checked = event.target.checked;
-
-      if (url.indexOf("?") === -1) {
-        url += "?";
+      if (isOptionEnabled) {
+        $option.prop("checked", true);
+        options[name] = JSON.parse($option.attr("data-value"));
       }
 
-      url = url.replace("&" + name + "=" + !checked, "");
-      url += "&" + name + "=" + checked;
+      $option.change(function(event) {
+        var url = window.location.href;
+        var checked = event.target.checked;
 
-      window.location.href = url;
+        if (url.indexOf("?") === -1) {
+          url += "?";
+        }
+
+        url = url.replace("&" + name + "=" + !checked, "");
+        url += "&" + name + "=" + checked;
+
+        window.location.href = url;
+      });
     });
-  });
 
-  var enableMathQuillFormulaAuthoring = window.mathquill4quill();
-  var quill = new Quill("#editor", quillOptions);
-  enableMathQuillFormulaAuthoring(quill, options);
+    var enableMathQuillFormulaAuthoring = window.mathquill4quill();
+    var quill = new Quill("#editor", quillOptions);
+    enableMathQuillFormulaAuthoring(quill, options);
+  });
 })(window.jQuery, window.Quill);
