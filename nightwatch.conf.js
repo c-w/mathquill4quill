742a8a40c637cb0397ef5b29a4f9a997b375fe6b
Clemens Wolff <clewolff@microsoft.com>
2019-06-26 11:50:13 -0400

    Fix chromedriver path reference on Windows

---
+++ nightwatch.conf.js
@@ -0,0 +1,21 @@
+/* eslint-env node */
+
+var chromedriver = require("chromedriver");
+
+module.exports = {
+  test_settings: {
+    chrome: {
+      webdriver: {
+        start_process: true,
+        server_path: chromedriver.path,
+        port: 9515
+      },
+      desiredCapabilities: {
+        browserName: "chrome",
+        chromeOptions: {
+          args: ["headless", "acceptInsecureCerts"]
+        }
+      }
+    }
+  }
+};
