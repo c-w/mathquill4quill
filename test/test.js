80b6e87e527c3137f1ad1e4375a1a853bed46687
Clemens Wolff <clewolff@microsoft.com>
2019-06-26 11:14:56 -0400

    Make eslint and prettier cover tests

--- test/test.js
+++ test/test.js
@@ -1,25 +1,35 @@
+/* eslint-env node */
+
 module.exports = {
-  'Is the formula editor visible' : function (browser) {
+  "Is the formula editor visible": function(browser) {
     browser
-    .useXpath()
-    .url('http://localhost:8080')
-    .waitForElementVisible('//*[@id="editor"]')
-    .click('//button[@class="ql-formula"]')
-    .waitForElementVisible('//div[@data-mode="formula"]');
+      .useXpath()
+      .url("http://localhost:8080")
+      .waitForElementVisible('//*[@id="editor"]')
+      .click('//button[@class="ql-formula"]')
+      .waitForElementVisible('//div[@data-mode="formula"]');
   },
-  'Is the formula editor using mathquill' : function (browser) {
+  "Is the formula editor using mathquill": function(browser) {
     browser
-    .useXpath()
-    .assert.attributeContains('//div[@data-mode="formula"]/span', 'class', 'mq-editable-field mq-math-mode');
+      .useXpath()
+      .assert.attributeContains(
+        '//div[@data-mode="formula"]/span',
+        "class",
+        "mq-editable-field mq-math-mode"
+      );
   },
-  'Can an equation be inserted' : function (browser) {
+  "Can an equation be inserted": function(browser) {
     browser
-    .useXpath()
-    .click('//div[@data-mode="formula"]/span')
-    .keys(['x','^','2'])
-    .click('//a[@class="ql-action"]')
-    .waitForElementVisible('//span[@class="ql-formula"]')
-    .assert.attributeContains('//span[@class="ql-formula"]', 'data-value', 'x^2')
-    .end();
+      .useXpath()
+      .click('//div[@data-mode="formula"]/span')
+      .keys(["x", "^", "2"])
+      .click('//a[@class="ql-action"]')
+      .waitForElementVisible('//span[@class="ql-formula"]')
+      .assert.attributeContains(
+        '//span[@class="ql-formula"]',
+        "data-value",
+        "x^2"
+      )
+      .end();
   }
 };
