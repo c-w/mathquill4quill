985492036c347dded6a35f48c8b269eddd3768be
Aiden Grossman <agrossman154@yahoo.com>
2019-06-25 22:15:38 -0700

    Added in basic functionality test for seeing if typing in an equation and hitting save adds it in to the quill editor

--- test/test.js
+++ test/test.js
@@ -1,11 +1,25 @@
 module.exports = {
-  'Demo test Google' : function (browser) {
+  'Is the formula editor visible' : function (browser) {
     browser
     .useXpath()
     .url('http://localhost:8080')
     .waitForElementVisible('//*[@id="editor"]')
     .click('//button[@class="ql-formula"]')
-    .waitForElementVisible('//div[@data-mode="formula"]')
+    .waitForElementVisible('//div[@data-mode="formula"]');
+  },
+  'Is the formula editor using mathquill' : function (browser) {
+    browser
+    .useXpath()
+    .assert.attributeContains('//div[@data-mode="formula"]/span', 'class', 'mq-editable-field mq-math-mode');
+  },
+  'Can an equation be inserted' : function (browser) {
+    browser
+    .useXpath()
+    .click('//div[@data-mode="formula"]/span')
+    .keys(['x','^','2'])
+    .click('//a[@class="ql-action"]')
+    .waitForElementVisible('//span[@class="ql-formula"]')
+    .assert.attributeContains('//span[@class="ql-formula"]', 'data-value', 'x^2')
     .end();
   }
 };
