82e2bd8a5db2211ea7b6977cc7e6c238d1ca83b3
Clemens Wolff <clewolff@microsoft.com>
2020-01-21 12:48:25 -0500

    Enable saving formula by hitting the enter key (#31)
    * Split tests into multiple suites
    
    * Enable saving formula by hitting the enter key

---
+++ tests/enter.test.js
@@ -0,0 +1,24 @@
+/* eslint-env node */
+
+module.exports = {
+  "Is the formula editor visible": function(browser) {
+    browser
+      .useXpath()
+      .url("http://localhost:8000")
+      .waitForElementVisible('//*[@id="editor"]')
+      .click('//button[@class="ql-formula"]')
+      .waitForElementVisible('//div[@data-mode="formula"]');
+  },
+  "Can an equation be inserted": function(browser) {
+    browser
+      .useXpath()
+      .keys(["x", "^", "3", browser.Keys.RETURN])
+      .waitForElementVisible('//span[@class="ql-formula"]')
+      .assert.attributeContains(
+        '//span[@class="ql-formula"]',
+        "data-value",
+        "x^3"
+      )
+      .end();
+  }
+};
