573636f8f0f97b666d5d92b21e55d6e9deda0f89
boomanaiden154 <39388941+boomanaiden154@users.noreply.github.com>
2020-02-26 18:23:36 -0800

    Enable passing through mathquill configuration (#38)
    * Added in mathquill config option
    
    * Added in support for user-defined handlers
    
    * Fixed code formatting and added rule to eslint for object spreading
    
    * Changed chromedriver version

--- mathquill4quill.js
+++ mathquill4quill.js
@@ -84,6 +84,8 @@ window.mathquill4quill = function(dependencies) {
 
     function newMathquillInput() {
       const autofocus = options.autofocus == null ? true : options.autofocus;
+      const mathQuillConfig =
+        options.mathQuillConfig != null ? options.mathQuillConfig : {};
       const cacheKey = options.cacheKey || "__mathquill4quill_cache__";
       let mqInput = null;
       let mqField = null;
@@ -98,18 +100,23 @@ window.mathquill4quill = function(dependencies) {
       }
 
       function syncMathquillToQuill(latexInput, saveButton) {
-        const mqField = MathQuill.getInterface(2).MathField(mqInput, {
-          handlers: {
-            edit() {
-              const latex = mqField.latex();
-              latexInput.value = latex;
-              setCacheItem(cacheKey, latex);
-            },
-            enter() {
-              saveButton.click();
-            }
+        const handlers =
+          mathQuillConfig.handlers == null ? {} : mathQuillConfig.handlers;
+        mathQuillConfig.handlers = {
+          ...handlers,
+          edit() {
+            const latex = mqField.latex();
+            latexInput.value = latex;
+            setCacheItem(cacheKey, latex);
+          },
+          enter() {
+            saveButton.click();
           }
-        });
+        };
+        const mqField = MathQuill.getInterface(2).MathField(
+          mqInput,
+          mathQuillConfig
+        );
 
         const cachedLatex = getCacheItem(cacheKey);
         if (cachedLatex) {
