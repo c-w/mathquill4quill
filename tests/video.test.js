c93954036a80a89463013c8042e3d68000980649
Clemens Wolff <clewolff@microsoft.com>
2020-03-24 10:58:33 -0400

    Fix missing video input after opening formula

---
+++ tests/video.test.js
@@ -0,0 +1,15 @@
+/* eslint-env node */
+
+module.exports = {
+  "Is the video input visible after opening the formula": function(browser) {
+    browser
+      .useXpath()
+      .url("http://localhost:8000")
+      .waitForElementVisible('//*[@id="editor"]')
+      .click('//button[@class="ql-formula"]')
+      .waitForElementVisible('//div[@data-mode="formula"]')
+      .click('//button[@class="ql-video"]')
+      .waitForElementVisible('//div[@data-mode="video"]')
+      .assert.visible('//div[@data-mode="video"]/input');
+  }
+};
