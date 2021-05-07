de8899a0bac8d9f17ca81e8868c13e34a6c1ecd8
Ahmed Mohamadeen <ah.mohamadeen@gmail.com>
2021-05-07 14:09:59 +0200

    Add tests for operator buttons both command and latex string

--- mathquill4quill.js
+++ mathquill4quill.js
@@ -222,6 +222,7 @@ window.mathquill4quill = function(dependencies) {
 
         const button = document.createElement("button");
         button.setAttribute("type", "button");
+        button.setAttribute("data-value", operator);
 
         katex.render(displayOperator, button, {
           throwOnError: false
