f73964e10fd6a84b7dc4e26d73bba55cd153f3d7
c-w <c-w@users.noreply.github.com>
2023-02-16 09:31:32 +0000

    deploy: e22d01705a0ec83ded264963ce07d5764e47fd8c

---
+++ nextjs/_next/static/K4Fj6nr_x9t5cIgC2BWli/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-156c2fd1112d1459.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
