d25555e7e00f110bae96bf9dd26f92c5192ea470
Clemens Wolff <clemens@justamouse.com>
2023-10-04 20:29:46 +0200

    Fix deploy

--- vuejs/vite.config.js
+++ vuejs/vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  base: "/mathquill4quill/vuejs/",
 });
