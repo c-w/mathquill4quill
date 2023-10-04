6c95494868281787da2f51aeaad2a31d3b6fe397
Clemens Wolff <clemens@justamouse.com>
2023-10-04 20:00:48 +0200

    Align demo UI, format and integration with build

--- vuejs/src/App.vue
+++ vuejs/src/App.vue
@@ -1,39 +1,37 @@
 <script setup>
-import './jquery'
+import "./jquery";
 // KaTeX dependency
-import katex from 'katex';
+import katex from "katex";
 window.katex = katex;
 
-import { QuillEditor, Quill } from '@vueup/vue-quill';
+import { QuillEditor, Quill } from "@vueup/vue-quill";
 
-
-import '@edtr-io/mathquill/build/mathquill.js';
-import '@edtr-io/mathquill/build/mathquill.css';
+import "@edtr-io/mathquill/build/mathquill.js";
+import "@edtr-io/mathquill/build/mathquill.css";
 
 // mathquill4quill include
-import mathquill4quill from 'mathquill4quill';
-import 'mathquill4quill/mathquill4quill.css';
+import mathquill4quill from "mathquill4quill";
+import "mathquill4quill/mathquill4quill.css";
 
-import { onMounted, ref } from 'vue';
+import { onMounted, ref } from "vue";
 
 const operators = [
-  ['\\pm', '\\pm'],
-  ['\\sqrt{x}', '\\sqrt'],
-  ['\\sqrt[3]{x}', '\\sqrt[3]{}'],
-  ['\\sqrt[n]{x}', '\\nthroot'],
-  ['\\frac{x}{y}', '\\frac'],
-  ['\\sum^{s}_{x}{d}', '\\sum'],
-  ['\\prod^{s}_{x}{d}', '\\prod'],
-  ['\\coprod^{s}_{x}{d}', '\\coprod'],
-  ['\\int^{s}_{x}{d}', '\\int'],
-  ['\\binom{n}{k}', '\\binom'],
+  ["\\pm", "\\pm"],
+  ["\\sqrt{x}", "\\sqrt"],
+  ["\\sqrt[3]{x}", "\\sqrt[3]{}"],
+  ["\\sqrt[n]{x}", "\\nthroot"],
+  ["\\frac{x}{y}", "\\frac"],
+  ["\\sum^{s}_{x}{d}", "\\sum"],
+  ["\\prod^{s}_{x}{d}", "\\prod"],
+  ["\\coprod^{s}_{x}{d}", "\\coprod"],
+  ["\\int^{s}_{x}{d}", "\\int"],
+  ["\\binom{n}{k}", "\\binom"]
 ];
 
 const quill = ref();
-const content = ref('');
+const content = ref("");
 
 onMounted(() => {
-
   // For this line, refer to the following issue comment:
   // https://github.com/c-w/mathquill4quill/issues/97#issuecomment-1740482856
   quill.value.container = quill.value.getEditor();
@@ -41,17 +39,13 @@ onMounted(() => {
   const enableMathQuillFormulaAuthoring = mathquill4quill({ Quill, katex });
 
   enableMathQuillFormulaAuthoring(quill.value, {
-    operators,
+    operators
   });
 });
-
-const textChange = (prop) => {
-  console.log('textChange', content.value);
-};
 </script>
 
 <template>
-   <div class="wrapper">
+  <main class="demo-container">
     <QuillEditor
       id="quill-editor"
       v-model:content="content"
@@ -61,18 +55,74 @@ const textChange = (prop) => {
       :options="{
         modules: {
           formula: true,
-          toolbar: [['bold', 'italic', 'underline'], ['formula']],
-        },
+          toolbar: [['bold', 'italic', 'underline'], ['formula']]
+        }
       }"
-      @editorChange="textChange"
     />
-  </div>
+
+    <footer>
+      <a href="https://github.com/c-w/mathquill4quill">
+        <p>Fork me on Github</p>
+        <img
+          width="149"
+          height="149"
+          src="https://github.blog/wp-content/uploads/2008/12/forkme_right_darkblue_121621.png?resize=149%2C149"
+          alt="Fork me on GitHub"
+        />
+      </a>
+    </footer>
+  </main>
 </template>
 
 <style scoped>
-.wrapper {
-  width: 80%;
-  margin: 50px auto;
-  height: 100%;
+footer {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  background-color: #121621;
+  text-align: center;
+}
+
+footer a {
+  color: #fff;
+  font-family: sans-serif;
+}
+
+footer p {
+  margin: 0.5em;
+}
+
+footer img {
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 99999;
+  display: none;
+}
+
+main {
+  width: 100%;
+  height: 200px;
+}
+
+@media (min-width: 500px) {
+  main {
+    max-width: 400px;
+    margin: 50px auto;
+  }
+
+  footer p {
+    display: none;
+  }
+
+  footer img {
+    display: block;
+  }
+}
+
+label {
+  padding-top: 0.5em;
+  display: block;
 }
 </style>
--- vuejs/src/main.js
+++ vuejs/src/main.js
@@ -1,10 +1,10 @@
-import { createApp } from 'vue'
-import 'katex/dist/katex.css';
-import '@vueup/vue-quill/dist/vue-quill.bubble.css';
-import '@vueup/vue-quill/dist/vue-quill.snow.css';
+import { createApp } from "vue";
+import "katex/dist/katex.css";
+import "@vueup/vue-quill/dist/vue-quill.bubble.css";
+import "@vueup/vue-quill/dist/vue-quill.snow.css";
 
-import 'mathquill4quill/mathquill4quill.css';
+import "mathquill4quill/mathquill4quill.css";
 
-import App from './App.vue'
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
