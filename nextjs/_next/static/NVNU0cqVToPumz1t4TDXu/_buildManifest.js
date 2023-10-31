5ed2902b8059b5339520f822cedab86753d31732
c-w <c-w@users.noreply.github.com>
2023-10-31 19:32:57 +0000

    deploy: e78be296c60f4aa528d329eac56c197043775759

--- nextjs/_next/static/NVNU0cqVToPumz1t4TDXu/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-156c2fd1112d1459.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ nextjs/_next/static/fgtJb3IHMrZyjn2R2eJse/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-6caa8feacfccd4bb.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
